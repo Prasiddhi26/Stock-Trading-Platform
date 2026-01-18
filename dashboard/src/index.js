b44f23e3feeb6e37a151a2dbb9f229171f995743
Web-Dev student <gitcode143@gmail.com>
2026-01-18 13:07:10 +0530

    Initial commit

---
+++ dashboard/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.css";
+import Home from "./components/Home";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<Home />} />
+      {/* //adding the star means that the changes happen only within the component and whole page wont get affected */}
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
